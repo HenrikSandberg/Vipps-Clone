c9e31c7b550a26d5b793bd2467eb19b785cd0b77
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2020-08-10 20:54:27 +0200

    Initial Commit

---
+++ Shared/Vipps_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vipps_CloneApp.swift
+//  Shared
+//
+//  Created by Henrik Anthony Odden Sandberg on 10/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct Vipps_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
